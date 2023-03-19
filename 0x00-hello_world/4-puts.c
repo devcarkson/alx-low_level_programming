f70d9bdef2b745eb442b69bdf11a114e79e7b5fb
devcarkson <devcarkson@gmail.com>
2023-03-19 10:03:57 -0700

    task done

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program to prints a line of code with a puts function
+ * Return: 0
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
