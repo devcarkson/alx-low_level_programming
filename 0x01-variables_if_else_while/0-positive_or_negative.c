a381c9d5290b35d4a98b1049f03742c9fc4695b2
devcarkson <devcarkson@gmail.com>
2023-03-20 13:52:32 -0700

    task done

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,7 +4,7 @@
 /**
  * main - Entry point
  * Discription: 'to check positive, negative or zero'
- * Return: always 0
+ * Return: always 0 (Success)
  */
 int main(void)
 {
@@ -14,15 +14,15 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
-                printf("%d is positive\n", n);
-    	}
+	printf("%d is positive\n", n);
+	}
 	else if (n == 0)
-    	{
-                printf("%d is zero\n", n);
-    	}
+	{
+	printf("%d is zero\n", n);
+	}
 	else
-    	{
-                printf("%d is negative\n", n);
-    	}
+	{
+	printf("%d is negative\n", n);
+	}
 	return (0);
 }
